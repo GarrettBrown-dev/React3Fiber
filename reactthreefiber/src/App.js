2f98d833eefaa417d6708838cb93d4274e803686
Garrett <gman9mm@live.com>
2021-07-08 14:01:26 -0700

    Box is now centered and rotates. Box is now in a variable instead of being directly placed in Canvas.

--- reactthreefiber/src/App.js
+++ reactthreefiber/src/App.js
@@ -1,15 +1,23 @@
 import "./App.scss";
-import React from "react";
-import { Canvas } from "react-three-fiber";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} /> //x,y,z
+      <meshStandardMaterial attach="material" />
+    </mesh>
+  );
+};
 
 function App() {
   return (
     <>
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} /> //x,y,z
-          <meshStandardMaterial attach="material" />
-        </mesh>
+        <Box />
       </Canvas>
     </>
   );
