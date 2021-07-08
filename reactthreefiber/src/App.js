b3b7be1689fd6070c529bd33e4e695ee7b0fdd7c
Garrett <gman9mm@live.com>
2021-07-08 13:39:45 -0700

    Add a small black square to top of screen.

--- reactthreefiber/src/App.js
+++ reactthreefiber/src/App.js
@@ -1,7 +1,18 @@
 import "./App.scss";
+import React from "react";
+import { Canvas } from "react-three-fiber";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} /> //x,y,z
+          <meshStandardMaterial attach="material" />
+        </mesh>
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
