2614a90eb6f3d11ce5be7d28f82304124107390f
Garrett <gman9mm@live.com>
2021-07-16 13:51:52 -0700

    Add arguments to change size of middle block.

--- reactthreefiber/src/App.js
+++ reactthreefiber/src/App.js
@@ -2,12 +2,12 @@ import "./App.scss";
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
 
-const SpinningMesh = ({ position }) => {
+const SpinningMesh = ({ position, args }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
     <mesh position={position} ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} /> //x,y,z
+      <boxBufferGeometry attach="geometry" args={args} /> //x,y,z
       <meshStandardMaterial attach="material" color="blue" />
     </mesh>
   );
@@ -18,7 +18,7 @@ function App() {
     <>
       <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
         <ambientLight intensity={0.3} />
-        <SpinningMesh position={[0, 1, 0]} />
+        <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} />
         <SpinningMesh position={[-2, 1, -5]} />
         <SpinningMesh position={[5, 1, -2]} />
       </Canvas>
