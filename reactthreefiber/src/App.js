ec84636611d705c2faec5e80e5de92bf1efb4b18
Garrett <gman9mm@live.com>
2021-07-16 15:09:42 -0700

    Running into trouble with shadows not casting. done for the day.

--- reactthreefiber/src/App.js
+++ reactthreefiber/src/App.js
@@ -6,8 +6,8 @@ const SpinningMesh = ({ position, args, color }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
-    <mesh position={position} ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={args} /> //x,y,z
+    <mesh castShadow={[true]} position={position} ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={args} />
       <meshStandardMaterial attach="material" color={color} />
     </mesh>
   );
@@ -16,9 +16,14 @@ const SpinningMesh = ({ position, args, color }) => {
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
+      <Canvas
+        shadowMap
+        colorManagement
+        camera={{ position: [-5, 2, 10], fov: 60 }}
+      >
         <ambientLight intensity={0.3} />
         <directionalLight
+          castShadow
           position={[0, 10, 0]}
           intensity={1.5}
           shadow-mapSize-width={1024}
@@ -31,9 +36,21 @@ function App() {
         />
         <pointLight position={[-10, 0, -20]} intensity={0.5} />
         <pointLight position={[0, -10, 0]} intensity={1.5} />
+
+        <group>
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            recieveShadow
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" />
+          </mesh>
+        </group>
+
         <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="skyblue" />
-        <SpinningMesh position={[-2, 1, -5]} color="lightpurple" />
-        <SpinningMesh position={[5, 1, -2]} color="lightpurple" />
+        <SpinningMesh position={[-2, 1, -5]} color="purple" />
+        <SpinningMesh position={[5, 1, -2]} color="purple" />
       </Canvas>
     </>
   );
